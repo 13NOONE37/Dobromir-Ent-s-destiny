2edc40d15befdde94f00007174392307abaacf87
13NOONE37 <oliwerklauze22@gmail.com>
2021-10-17 13:03:12 +0200

    template done

--- src/script.js
+++ src/script.js
@@ -1,96 +1,77 @@
-import './style.css'
-import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import * as dat from 'dat.gui'
-import testVertexShader from './shaders/test/vertex.glsl'
-import testFragmentShader from './shaders/test/fragment.glsl'
+import "./style.css";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import * as dat from "dat.gui";
 
-/**
- * Base
- */
-// Debug
-const gui = new dat.GUI()
+//Base
 
-// Canvas
-const canvas = document.querySelector('canvas.webgl')
+//Debug
+const gui = new dat.GUI();
 
-// Scene
-const scene = new THREE.Scene()
+//Canvas
+const canvas = document.querySelector("canvas.webgl");
 
-/**
- * Test mesh
- */
-// Geometry
-const geometry = new THREE.PlaneBufferGeometry(1, 1, 32, 32)
+//Scene
+const scene = new THREE.Scene();
 
-// Material
-const material = new THREE.ShaderMaterial({
-    vertexShader: testVertexShader,
-    fragmentShader: testFragmentShader,
-    side: THREE.DoubleSide
-})
+//Test
+const boxGeometry = new THREE.BoxBufferGeometry(1, 1, 1);
+const boxMaterial = new THREE.MeshStandardMaterial({ color: 0xffff55 });
+const box = new THREE.Mesh(boxGeometry, boxMaterial);
+scene.add(box);
 
-// Mesh
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+const light = new THREE.AmbientLight(0xffffff, 50);
+light.position.set(5, 5, 5);
+scene.add(light);
 
-/**
- * Sizes
- */
+//Sizes
 const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
-}
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
 
-window.addEventListener('resize', () =>
-{
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
+window.addEventListener("resize", () => {
+  //Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
 
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
+  //Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
 
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-})
+  //Update renderer
+});
 
-/**
- * Camera
- */
-// Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.set(0.25, - 0.25, 1)
-scene.add(camera)
+//Camera
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  1000
+);
+camera.position.set(0, 15, 15);
+scene.add(camera);
 
-// Controls
-const controls = new OrbitControls(camera, canvas)
-controls.enableDamping = true
+//Controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
 
-/**
- * Renderer
- */
+//Renderer
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
-})
-renderer.setSize(sizes.width, sizes.height)
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
-/**
- * Animate
- */
-const tick = () =>
-{
-    // Update controls
-    controls.update()
+//Animate
+const tick = () => {
+  //Update controls
+  controls.update();
 
-    // Render
-    renderer.render(scene, camera)
+  //Render
+  renderer.render(scene, camera);
 
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
-}
-
-tick()
+  //Call tick again on next frame
+  window.requestAnimationFrame(tick);
+};
+tick();
